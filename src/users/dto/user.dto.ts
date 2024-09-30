1583f009db7a63d7c3bf402af8537c68d760d1d1
Yolosopher <nika.nishnianidze.10@gmail.com>
2024-09-30 23:43:58 +0400

    Refactor user DTO with class-validator decorators

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,13 +1,26 @@
 import { Role } from '@prisma/client';
+import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
   name: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/^(?=.*[0-9])/, {
+    message: 'Password must contain at least one number',
+  })
   password: string;
 }
 
 export class LoginUserDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
   password: string;
 }
 
